ca7c6ac8b76f7fad14150dae84e9bf14f6d8fabf
jb-network <85036487+jb-network@users.noreply.github.com>
2022-05-19 18:50:42 -0500

    This is a simple program for the Level 8  "The Defense of Consolas" in the "C# Players Guide".
    Assumes an 8x8 defense grid
    
    Challenge Requires:
    1) Beep sound
    2) Console color change
    3) Four coordinates provided

--- The_Defense_of_Consolas/Program.cs
+++ The_Defense_of_Consolas/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This is challenge work for the "C# Players Guide"
+// Level 8 The Defense of Consolas Challenge
+//This is challenge assumes an 8X8 Grid
+//The challenge requires a console color change and a beep. 
+
+//Set the console color to green
+Console.ForegroundColor = ConsoleColor.Green;
+
+//Collect the expected attack coordinates (Row and Column) based on the scenario
+Console.WriteLine("---------------Defense Coordination Calculator--------------------");
+Console.Write("Enter the Target Row: ");
+int targetRow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the Target Column: ");
+int targetColumn = Convert.ToInt32(Console.ReadLine());
+
+
+//Change the console color
+Console.ForegroundColor = ConsoleColor.Red;
+
+//Provide four defense position squares that surround the provided attack coordinates
+Console.WriteLine("___________________________________________________________________");
+Console.WriteLine("Move the following locations: ");
+Console.WriteLine($"First Position: ({targetRow}, {targetColumn - 1})");
+Console.WriteLine($"Second Position: ({targetRow}, {targetColumn + 1})");
+Console.WriteLine($"Third Position: ({targetRow - 1}, {targetColumn})");
+Console.WriteLine($"Fourth Position: ({targetRow + 1}, {targetColumn})");
+Console.WriteLine("___________________________________________________________________");
+
+//Add Console Beep sound
+Console.Beep(450, 800);
+
+
+//Restore the console color back to green
+Console.ForegroundColor = ConsoleColor.Green;
+
+
+
+
